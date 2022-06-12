1d65aeaa7d0052d641cd7033ef97b0391e64180d
yanjustino <contato@yanjustino.com>
2022-06-12 10:55:35 -0300

    Project 01

--- nd1309_practice_block/block.js
+++ nd1309_practice_block/block.js
@@ -1,13 +1,6 @@
-/**
- * Import crypto-js/SHA256 library
- */
 const SHA256 = require('crypto-js/sha256');
 
-/**
- * Class with a constructor for block 
- */
 class Block {
-
   constructor(data) {
     this.id = 0;
     this.nonce = 144444;
@@ -15,16 +8,7 @@ class Block {
     this.hash = "";
   }
 
-  /**
-   * Step 1. Implement `generateHash()`
-   * method that return the `self` block with the hash.
-   * 
-   * Create a Promise that resolve with `self` after you create 
-   * the hash of the object and assigned to the hash property `self.hash = ...`
-   */
-  // 
   generateHash() {
-    // Use this to create a temporary reference of the class object
     let self = this;
 
     var promise = new Promise(function (resolve, reject) {
@@ -41,5 +25,4 @@ class Block {
   }
 }
 
-// Exporting the class Block to be reuse in other files
 module.exports.Block = Block;
